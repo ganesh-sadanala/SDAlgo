75c8a32fc22b20620a6d8a3c0e8a2c92e2b20556
gsadanala <ganesh8374387106@gmail.com>
2024-04-26 21:05:40 -0400

    Update PrefixTree.java

--- src/SearchAlgo/PrefixTree.java
+++ src/SearchAlgo/PrefixTree.java
@@ -52,4 +52,55 @@ class PrefixTree {
             collectSuggestions(entry.getValue(), prefix + entry.getKey(), suggestions);
         }
     }
+
+    /*
+    // Efficient way of suggesting by storing words in each node
+    // Priority can be applied many to show top 5 related words based
+    // on: lexicographical, rating/popularity, most visited by counter
+    class TrieNode{
+        TrieNode []children;
+        PriorityQueue<String> pq;
+
+        public TrieNode(){
+            children=new TrieNode[26];
+            pq=new PriorityQueue<>((a, b) -> b.compareTo(a));
+        }
+
+        void addToPQ(String word){
+            pq.add(word);
+            if(pq.size() > 3) pq.poll();
+        }
+
+        List<String> getTopThree(){
+            List<String> topThree=new ArrayList<>();
+            while(!pq.isEmpty()) topThree.add(pq.poll());
+            Collections.reverse(topThree);
+            return topThree;
+        }
+    }
+
+    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+        TrieNode root=new TrieNode();
+        for(String product:products) insert(root, product);
+
+        List<List<String>> results=new ArrayList<>();
+        for(char c:searchWord.toCharArray()){
+            if((root=root.children[c-'a']) == null) break;
+            results.add(root.getTopThree());
+        }
+
+        while(results.size() < searchWord.length())
+            results.add(new ArrayList<>());
+
+        return results;
+    }
+
+    void insert(TrieNode root, String word){
+        for(char c:word.toCharArray()){
+            if(root.children[c-'a']==null) root.children[c-'a']=new TrieNode();
+            root=root.children[c-'a'];
+            root.addToPQ(word);
+        }
+    }
+     */
 }
